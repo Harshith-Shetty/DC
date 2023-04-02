48d1f08fafd750ea4d560a3b608b86bbf30c684d
Harshith Shetty <56068877+Harshith-Shetty@users.noreply.github.com>
2023-04-02 11:43:49 +0000

    exp 3 update

--- Experiment 3/MyClient.java
+++ Experiment 3/MyClient.java
@@ -1,9 +1,15 @@
 import java.rmi.*;
+import java.util.*;
 public class MyClient {
   public static void main(String args[]) {
     try {
       Adder stub = (Adder) Naming.lookup("rmi://localhost:5000/sonoo");
-      System.out.println(stub.add(34, 62));
+      System.out.println("Enter the Number 1");
+      n1=sc.nextInt();
+      System.out.println("Enter the Number 2");
+      n2=sc.nextInt();
+      System.out.println("Addition = "+(stub.add(n1, n2)));
+
     } catch (Exception e) {}
   }
 }
